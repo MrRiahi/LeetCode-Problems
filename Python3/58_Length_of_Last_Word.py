af3f37d60fa11af21bc55c6723612921c06cdf8a
riahi <mohammad.r.riahi@gmail.com>
2022-04-20 16:29:59 +0430

    Add 58_Length_of_Last_Word solution

---
+++ Python3/58_Length_of_Last_Word.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        """
+        First solution.
+        :param s: 
+        :return: 
+        """""
+        words = s.split(' ')
+
+        if len(words) == 1:
+            return len(words[0])
+
+        for i in range(len(words) - 1, -1, -1):
+            if len(words[i]) != 0:
+                return len(words[i])
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        """
+        Second solution.
+        :param s:
+        :return:
+        """
+
+        return len(s.rstrip().split(' ')[-1])

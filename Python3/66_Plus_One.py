7b10990f606f99fcdd03a73f429b4ee0578e9eb4
MrRiahi <mohammad.r.riahi@gmail.com>
2022-04-22 21:08:35 +0430

    Add 66_Plus_One solution

---
+++ Python3/66_Plus_One.py
@@ -0,0 +1,6 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        number = int(''.join([str(digit) for digit in digits])) + 1
+        
+        return list(str(number))
